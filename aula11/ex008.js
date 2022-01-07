8f92020e5ed1b5688ef107a52c388b1f96560aba
Samille Machado <63076709+samillemachado@users.noreply.github.com>
2022-01-07 18:33:51 -0300

    exercicios condicionais

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 58
+console.log(`A velocidade do seu carro é de ${vel}Km/hr.`)
+if (vel > 60) { //condição simples
+    console.log('Você ultrapassou a velocidade permitida! MULTADO!')
+}
+console.log('Dirija sempre usando o cinto de segurança!')
