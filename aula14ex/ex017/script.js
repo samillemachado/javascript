7e085e9b9d17d72fbb15a569c6aa0b087b933864
Samille Machado <63076709+samillemachado@users.noreply.github.com>
2022-01-11 09:57:25 -0300

    exercicio ex017

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,17 @@
-alert('Olá!')
+function gerar(){
+var num = window.document.getElementById('txtnum')
+var tab = window.document.getElementById('seltab')
+var n = Number(num.value)
+
+if (n == 0 || num.value.length == 0){
+    window.alert('Digite um número!')
+} else{
+    tab.innerHTML = ''
+    for (c = 1; c <= 10; c++){
+        var item = document.createElement('option')
+        item.text = `${n} x ${c} = ${n*c}`
+        item.value = `tab${c}`
+        tab.appendChild(item)
+    }
+}
+}
